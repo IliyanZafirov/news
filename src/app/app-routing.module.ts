b28421f5928f6429a9edb47cb221174f7ca3eec2
Iliyan Zafirov <iliyanzafirov00@gmail.com>
2023-12-12 13:54:10 +0200

    remove lorem ipsum

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppModule } from './app.module';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
   {
     path: '', component: AppModule, children: [
       { path: '', component: HeaderComponent },
-      { path: '', component: FooterComponent }
     ]
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule,
