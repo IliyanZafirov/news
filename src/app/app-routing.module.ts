26074d8299df1e30473e6229d6311ea87504055f
Iliyan Zafirov <iliyanzafirov00@gmail.com>
2023-12-12 13:35:38 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppModule } from './app.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppModule, children: [
+      { path: '', component: HeaderComponent },
+      { path: '', component: FooterComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
